5a2a19a1f00408387a7306517deb99e1021a30f4
Rishi Kulkarni <rishi@kulkarni.science>
2022-03-14 21:06:14 -0400

    add collapse_hierarchy

--- hierarch/regression_utils.py
+++ hierarch/regression_utils.py
@@ -1,134 +1,129 @@
+from functools import lru_cache
+from itertools import repeat
+from typing import Tuple
 import numpy as np
 from numba import jit
 
-from .internal_functions import nb_unique
+
+@jit(nopython=True)
+def intercepts(X: np.ndarray, y: np.ndarray) -> np.ndarray:
+    return np.linalg.lstsq(X, y)[0]
 
 
-class GroupbyMean:
-    """Class for performing groupby reductions on numpy arrays.
+def encoded_last_level(X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """One-hot encoding of the last level of hierarchy.
 
-    Currently only supports mean reduction.
-    """
-
-    def __init__(self):
-
-        self.cache_dict = {}
-
-    def fit(self, reference_data):
-        """Fits the class to reference data.
-
-        Parameters
-        ----------
-        reference_data : 2D numeric numpy array
-            Reference data to use for the reduction.
-
-        """
-        self.reference_dict = {}
-
-        reference = reference_data[:, :-1]
-
-        for i in reversed(range(1, reference.shape[1])):
-
-            reference, counts = nb_unique(reference[:, :-1])[0::2]
-
-            self.reference_dict[i] = (reference, counts.astype(np.int64))
-
-    def transform(self, target, iterations=1):
-        """Performs iterative groupby reductions.
-
-        Parameters
-        ----------
-        target : 2D numeric array
-            Array to be reduced.
-        iterations : int, optional
-            Number of reductions to perform, by default 1
-
-        Returns
-        -------
-        2D numeric array
-            Array with the same number of rows as target data, but one fewer column
-            for each iteration. Final column values are combined by taking the mean.
-        """
-        for i in range(iterations):
-
-            key = hash((target[:, :-2]).tobytes())
-
-            try:
-
-                reduce_at_list, reduce_at_counts = self.cache_dict[key]
-
-            except KeyError:
-
-                column = target.shape[1] - 2
-
-                reference, counts = self.reference_dict[column]
-
-                reduce_at_list = class_make_ufunc_list(
-                    target[:, :-2], reference, counts
-                )
-
-                reduce_at_counts = (
-                    np.append(reduce_at_list[1:], target[:, -2].size) - reduce_at_list
-                )
-
-                self.cache_dict[key] = reduce_at_list, reduce_at_counts
-
-                if len(self.cache_dict.keys()) > 50:
-
-                    self.cache_dict.pop(list(self.cache_dict)[0])
-
-            agg_col = np.add.reduceat(target[:, -1], reduce_at_list) / reduce_at_counts
-
-            target = target[reduce_at_list][:, :-1]
-
-            target[:, -1] = agg_col
-
-        return target
-
-    def fit_transform(self, target, reference_data=None, iterations=1):
-        """Combines fit() and transform() for convenience. See those methods for details."""
-        if reference_data is None:
-            reference_data = target
-        self.fit(reference_data)
-        return self.transform(target, iterations=iterations)
-
-
-jit(nopython=True, cache=True)
-
-
-def class_make_ufunc_list(target, reference, counts):
-    """Makes a list of indices to perform a ufunc.reduceat operation along.
-
-    This is necessary when an aggregation operation is performed
-    while grouping by a column that was resampled. The target array
-    must be lexsorted.
+    Delegates to _encoded_last_level, which is cached.
 
     Parameters
     ----------
-    target : 2D numeric array
-        Array that the groupby-aggregate operation will be performed on.
-    reference : 2D numeric array
-        Unique rows in target for the column that will be aggregated to.
-    counts : 1D array of ints
-        Number of times each row in reference should appear in target.
+    X : np.ndarray
+        design matrix where each column is a level of
+        hierarchy
 
     Returns
     -------
-    1D array of ints
-        Indices to reduceat along.
+    Tuple[np.ndarray, np.ndarray]
+        one-hot encoded final level,
+        unique rows in the final level
+
+    Examples
+    --------
+
+    Consider a simple design matrix that has three units
+    in its lowest level:
+
+    >>> design = np.array([[1, 1], [1, 2], [1, 3]])
+    >>> encoded_last_level(design)
+    (array([[1., 0., 0.],
+           [0., 1., 0.],
+           [0., 0., 1.]]), array([[1, 1],
+           [1, 2],
+           [1, 3]]))
+
+    >>> repeated_design = design.repeat(2, axis=0)
+    >>> encoded_last_level(repeated_design)
+    (array([[1., 0., 0.],
+           [1., 0., 0.],
+           [0., 1., 0.],
+           [0., 1., 0.],
+           [0., 0., 1.],
+           [0., 0., 1.]]), array([[1., 1.],
+           [1., 2.],
+           [1., 3.]]))
     """
+    hashable = tuple(map(tuple, X.tolist()))
+    return _encoded_last_level(hashable)
 
-    ufunc_list = np.empty(len(reference), dtype=np.int64)
-    i = 0
 
-    if reference.shape[1] > 1:
-        for idx, _ in enumerate(ufunc_list):
-            ufunc_list[idx] = i
-            i += counts[np.all((reference == target[i]), axis=1)][0]
+@lru_cache
+def _encoded_last_level(X_hashable: Tuple[Tuple]) -> np.ndarray:
+    uniq, inverse = np.unique(X_hashable, axis=0, return_inverse=True)
+    return np.eye(len(uniq))[inverse], uniq
 
-    else:
-        for idx, _ in enumerate(ufunc_list):
-            ufunc_list[idx] = i
-            i += counts[(reference == target[i]).flatten()].item()
 
-    return ufunc_list
+def collapse_hierarchy(
+    design: np.ndarray, y: np.ndarray, weights: np.ndarray, levels_to_collapse: int
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Collapses a level of hierarchy by computing regression coefficients and
+    passing them up to the next level.
+
+    Parameters
+    ----------
+    design : np.ndarray
+    y : np.ndarray
+    weights : np.ndarray
+        Bootstrapped weights.
+    levels_to_collapse : int
+        Number of times to repeat the process.
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray]
+        Collapsed X and y
+
+    Examples
+    --------
+
+    This function implements a single step of two-stage regression.
+
+    >>> design = np.array([[1., 1.], [1., 2.], [1., 3.]]).repeat(2, axis=0)
+    >>> y = np.array([1., 2., 3., 4., 5., 6.])
+    >>> weights = np.array([1. for i in y])
+
+    Calling collapse_hierarchy with level_to_collapse=1 should yield us a design
+    matrix with a single column and y-values representing the average of every
+    pair of y-values.
+
+    >>> collapse_hierarchy(design, y, weights, 1)
+    (array([[1.],
+           [1.],
+           [1.]]), array([1.5, 3.5, 5.5]))
+
+    This function computes weighted averages for each intercept, as well.
+
+    >>> weights = np.array([2, 0, 2, 0, 2, 0])
+    >>> collapse_hierarchy(design, y, weights, 1)
+    (array([[1.],
+           [1.],
+           [1.]]), array([1.5, 3.5, 5.5]))
+
+    Several levels can be collapsed at once:
+    >>> design = np.array([[1, 1, 1], [1, 1, 2], [1, 2, 1], [1, 2, 2]])
+    >>> y = np.array([1., 2., 3., 4.])
+    >>> weights = np.array([1 for i in y])
+    >>> collapse_hierarchy(design, y, weights, 2)
+    (array([[1],
+           [1]]), array([1.5, 3.5]))
+
+    """
+    out_x = design
+    # this is only allowed when we're only computing
+    # intercepts - it's not a general approach to
+    # weighted regression
+    out_y = y * weights
+    for _ in repeat(None, levels_to_collapse):
+        regressor, out_x = encoded_last_level(out_x)
+        out_y = intercepts(regressor, out_y)
+        out_x = out_x[:, :-1]
+    return out_x, out_y
